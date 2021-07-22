8e8a3dc03496ee40db7a93325cbdad852b9b2989
Josiel Faria <josielsifa@gmail.com>
2021-07-22 13:47:56 -0300

    Removendo codigos nao utilizados

--- src/app/components/form-example/form-example.component.ts
+++ src/app/components/form-example/form-example.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-example',
   templateUrl: './form-example.component.html',
   styleUrls: ['./form-example.component.scss']
 })
-export class FormExampleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FormExampleComponent { }
